969e47642d004011eadf0b3daf19f8762708886c
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-22 03:11:39 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./assets/main.css";
 function App() {
   return (
     <div className="">
-      <h1 className="text-red-600">Hello World</h1>
+      <h1 className="text-red-600">Hello World!</h1>
     </div>
   );
 }
