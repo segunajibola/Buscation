a1b709696fb727df6773d6481c1b149e7a171822
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-27 05:33:23 +0100

    darkMode

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React from "react";
 import "./assets/main.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import popularDestinations from "./data/popularDestinations";
 import DestinationCards from "./components/DestinationCards";
 
+function darkMode() {
+    const btn = document.getElementById("dark");
+
+    btn.addEventListener("click", () => btn.classList.toggle("dark"))    
+}
+
 function App() {
   return (
-      <>
-        <div className="bg-gray-100 grid lg:grid-cols-2 2xl:grid-cols-5">
+      <div className="dark" id="dark">
+        <div className="bg-gray-100 dark:bg-gray-900 grid lg:grid-cols-2 2xl:grid-cols-5">
             <div className="px-4 py-4 max-w-md mx-auto sm:max-w-xl lg:px-12 lg:py-24 lg:max-w-full xl:mr-0 2xl:col-span-2">
               <div className="max-w-xl">
+                      <p className="text-right text-gray-900 dark:text-white" onClick={darkMode}>Dark mode</p>
+                      <FontAwesomeIcon icon="fa-solid fa-moon" />
                       <img className="w-28 h-28 inline-block" src="/images/buscation.png" alt="Buscation" />
-                      <h1 className="text-gray-900 inline-block tracking-wide font-bold text-5xl ml-5 text-brand">Buscation</h1>
+                      <h1 className="text-gray-900 dark:text-white inline-block tracking-wide font-bold text-5xl ml-5 ">Buscation</h1>
                       <img src="/images/bus.jpg" alt="People going in the bus" className="w-full h-52 my-2 rounded-lg shadow-xl sm:mt-8 sm:h-64 sm:object-cover object-center lg:hidden" />
-                      <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
+                      <h1 className="mt-6 text-2xl font-bold text-gray-900 dark:text-white sm:mt-8 sm:text-4xl lg:text-3xl xl:text-4xl">
                           You can travel anywhere. 
                           <br />
-                          <span className="text-indigo-500">Take advantage of that.</span>
+                          <span className="text-indigo-500 dark:text-indigo-600">Take advantage of that.</span>
                       </h1>
-                      <p className="mt-2 text-gray-600 sm:text-xl">
+                      <p className="mt-2 text-gray-600 dark:text-white sm:text-xl">
                           Buscation helps you tour beautiful locations so you can enjoy nature..
                       </p>
                       <div className="mt-4 sm:mt-6 space-x-1">
-                          <a href="#" className="inline-block px-5 py-3 uppercase tracking-wider font-semibold text-sm sm:text-base rounded-lg focus:outline-none focus:ring focus:ring-offset-2 bg-indigo-500 hover:bg-indigo-300 focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-600 text-white shadow-lg hover:-translate-y-0.5 transform  transition ">Join next tour</a>
+                          <a href="#" className="inline-block px-5 py-3 uppercase tracking-wider font-semibold text-sm sm:text-base rounded-lg focus:outline-none focus:ring focus:ring-offset-2 bg-indigo-500 dark:bg-indigo-600 hover:bg-indigo-300 focus:ring-indigo-500 focus:ring-opacity-50 active:bg-indigo-600 text-white dark:text-black shadow-lg hover:-translate-y-0.5 transform  transition ">Join next tour</a>
                           <a href="#" className="inline-block px-5 py-3 uppercase tracking-wider font-semibold text-sm sm:text-base rounded-lg focus:outline-none focus:ring focus:ring-offset-2 bg-gray-300 hover:bg-gray-200 focus:ring-gray-300 focus:ring-opacity-50 active:bg-gray-400 text-gray-800">Know more</a>
                       </div>
               </div>
@@ -49,7 +58,7 @@ function App() {
 
             </div> 
             </div>  
-      </>
+      </div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,11 @@
 module.exports = {
   purge: {
-    enabled: true,
-    content: ["./src/**/*.js", "./public/index.html"],
+    // enabled: true,
+    // content: ["./src/**/*.js", "./public/index.html"],
   },
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        brand: {
-        DEFAULT: "#0fa9e6",
-        light: "#3fbaeb",
-        dark: "#0c87b8",
-        },
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
