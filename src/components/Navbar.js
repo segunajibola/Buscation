0a246aba2da43367c8f122451d4126c0e6876d8b
Segun Ajibola <ajbl.segun@gmail.com>
2022-02-09 23:27:44 +0100

    editnav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+  const btn = document.querySelector("button.mobile-menu-button");
+  const menu = document.querySelector(".mobile-menu");
+
+  btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+  });
+
 const Navbar = () => {
   return (
-      <div className='max-w-xl p-3 m-auto flex justify-between m-5 text-xs'>
-        <div>
-          <Link className='flex'>Buscation</Link>
+    <div>
+      <nav className="bg-white shadow-lg">
+        <div className='max-w-xl p-3 m-auto flex justify-between text-xs'>
+          <div className='flex'>
+            <Link>Buscation</Link>
+          </div>
+          <div className='flex'>
+            <Link>Experience</Link>
+            <Link>Pickup Route</Link>
+            <Link>About</Link>
+            <Link>Work with us</Link>
+            <Link>Buscation Plus</Link>
+          </div>
+          <div className='flex'>
+            <Link>Sign-up</Link>
+            <Link>Log-in</Link>          
+          </div>
         </div>
-        <div>
-          <Link>Experience</Link>
-        <Link>Pickup Route</Link>
-        <Link>About</Link>
-        <Link>Work with us</Link>
-        <Link>Buscation Plus</Link>
-        </div>
-        <div>
-        <Link>Sign-up</Link>
-        <Link>Log-in</Link>          
-        </div>
-      </div>
+      </nav>
+
+</div>
   );
 };
 
