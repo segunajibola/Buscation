61d9aba189d8cc228716bfb4a39664987370ce87
Segun Ajibola <ajbl.segun@gmail.com>
2022-02-10 13:14:38 +0100

    addicons

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,11 @@ import DestinationCards from './DestinationCards';
 import popularDestinations from '../data/popularDestinations';
 import { Link } from 'react-router-dom';
 
+// scroll = () => { window.scrollTo(0, 0); }
+// function scroll() {
+//     window.scrollTo(0, 0);
+// };
+
 const Home = () => {
     return (
         <>
@@ -29,7 +34,6 @@ const Home = () => {
             </div>
             <div className="max-w-full lg:max-w-full mx-auto px-8 lg:px-8 py-4 dark:bg-gray-900">
             <h2 className="text-xl text-gray-900 dark:text-white text-center">Popular destination</h2>
-            <p className="mt-2 text-gray-600 dark:text-white text-center">Collecton of great places in each states.</p>
             {/* <div className='text-center'>
             <p className='mt-2 text-gray-600 dark:text-white inline-block border-2 border-red-500 p-3'>Sort By Location<FontAwesomeIcon className="text-xl text-black inline-block ml-2 -mb-1 text-right dark:text-white cursor-pointer" icon={faAngleDown} /></p>
             <label for="location">Sort by Location:</label>
@@ -54,7 +58,7 @@ const Home = () => {
                     {popularDestinations.filter(destination => destination.city === 'Lagos').map(destination => <DestinationCards destination={destination} key={destination.city} />)} */}
             </div>
             <div className='text-center'>
-                <Link to='/all-states'><button className='border-2 rounded-lg bg-indigo-400 border-indigo-800 hover:bg-indigo-500 px-2 py-1 m-5 text-2xl dark:text-white dark:border-white'>See all</button></Link>
+                <Link to='/all-states'><button className='border-2 rounded-lg bg-indigo-400 border-indigo-800 hover:bg-indigo-500 px-2 py-1 m-5 text-2xl dark:text-white dark:border-white' onClick={() => window.scrollTo(0, 0)}>See all</button></Link>
             </div>
     </div>  
         </>
