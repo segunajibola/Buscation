0d11284c526c6f6cf6e2921ff64255a9d232c170
Segun Ajibola <ajbl.segun@gmail.com>
2022-02-10 11:39:45 +0100

    addicons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -46,9 +46,11 @@ const Footer = () => {
                         <Link to="https://www.facebook.com/"><i className="fab fa-facebook text-blue-300 mr-2"></i><FontAwesomeIcon className="text-lg mt-1 mr-3 text-blue-800 dark:text-white cursor-pointer"  icon={faFacebook} /></Link>
                         <Link to="https://wa.me/"><i className="fab fa-whatsapp text-green-400 mr-2"></i><FontAwesomeIcon className="text-lg mt-1 mr-3 text-red-800 dark:text-white cursor-pointer"  icon={faInstagram} /></Link>
                         <Link to="https://www.instagram.com/"><i className="fab fa-instagram text-red-900 mr-2"></i><FontAwesomeIcon className="text-lg mt-1 mr-3 text-green-800 dark:text-white cursor-pointer"  icon={faWhatsapp} /></Link>
-                        <p>Download App</p>
-                        <p>Google App</p>
-                        <p>App Store</p>
+                        <p className='mt-4'>Download Our App</p>
+                        <div className=''>
+                        <Link><img src="/images/playstore.png" alt="People going in the bus" className="inline w-20 h-8" /></Link>
+                        <Link><img src="/images/appstore.png" alt="People going in the bus" className="inline w-26 h-11" /></Link>
+                        </div>
                     </div>
                 </div>
             </div>
