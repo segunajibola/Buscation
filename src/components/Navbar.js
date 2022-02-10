7356ab81a83681787ad15689254a3d2e4404ab8d
Segun Ajibola <ajbl.segun@gmail.com>
2022-02-10 05:08:38 +0100

    editnav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -57,7 +57,7 @@ const Navbar = () => {
             <div class="md:hidden flex items-center"> 
             <FontAwesomeIcon className="text-lg mt-1 mr-3 text-yellow-400 dark:text-white cursor-pointer" onClick={darkMode}  icon={faMoon} />
               <button className="outline-none mobile-menu-button">
-                <svg className=" w-6 h-6 text-gray-500 hover:text-green-500 " x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"> <path d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                <svg className="w-6 h-6 dark:text-indigo-500" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"> <path d="M4 6h16M4 12h16M4 18h16"></path></svg>
               </button>
 
             </div>
