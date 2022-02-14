ace1d381edf1218c2db848b0c58c1664da67f986
Segun Ajibola <ajbl.segun@gmail.com>
2022-02-14 03:30:50 +0100

    newEdits

--- src/components/DestinationCards.js
+++ src/components/DestinationCards.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const DestinationCards = ({ destination }) => {
     return (
         <>
-            <div className="flex items-center rounded-lg bg-white dark:bg-gray-800 shadow-lg overflow-hidden">
+            <div className="flex items-center rounded-lg bg-white dark:bg-gray-800 shadow-lg overflow-hidden hover:-translate-y-2.5 transform transition hover:scale-110">
                 <img src={destination.imageUrl} alt={destination.imageAlt} className="h-32 w-32 flex-shrink-0" />
                 <div className="px-6 py-4">
                     <h3 className="text-lg font-semibold text-gray-800 dark:text-white">{destination.city}</h3>
