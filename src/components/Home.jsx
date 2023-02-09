007ed08077b2d04633d6f6750d86ee9a2d109d77
Segun Ajibola <ajbl.segun@gmail.com>
2023-02-09 11:20:47 +0100

    addLekki

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,37 +5,38 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <>
-      <div className="grid bg-gray-100 pt-16 lg:pt-20 lg:pb-8 dark:bg-gray-700 lg:grid-cols-2 2xl:grid-cols-5">
-        <div className="mx-auto max-w-md px-4 py-4 sm:max-w-xl lg:max-w-full lg:px-12 lg:py-28 xl:mr-0 2xl:col-span-2">
-            <h1 className="mt-2 text-2xl font-bold text-gray-900 dark:text-white sm:mt-4 sm:text-4xl lg:mt-4 lg:text-3xl xl:text-4xl">
-              You can travel anywhere.
-              <br />
-              <span className="text-indigo-500 dark:text-indigo-600">
-                Take advantage of that.
-              </span>
-            </h1>
-            <p className="mt-2 text-gray-600 dark:text-white sm:text-xl">
-              Tourcation lets you tour beautiful locations so you can have enjoyable memories.
-            </p>
-            <img
-              src="/images/bus.jpg"
-              alt="People going in the bus"
-              className="my-2 h-52 w-full rounded-lg object-center shadow-xl sm:mt-8 sm:h-64 sm:object-cover lg:hidden"
-            />
-            <div className="mt-4 space-x-1 sm:mt-6">
-              <a
-                href="/"
-                className="focus:outline-none inline-block transform rounded-lg bg-indigo-500 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-indigo-400 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 focus:ring-offset-2 active:bg-indigo-600 dark:bg-indigo-600  sm:text-base "
-              >
-                Join next tour
-              </a>
-              <a
-                href="/"
-                className="focus:outline-none inline-block rounded-lg bg-gray-300 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-gray-800 hover:bg-gray-200 focus:ring focus:ring-gray-300 focus:ring-opacity-50 focus:ring-offset-2 active:bg-gray-400 sm:text-base"
-              >
-                Know more
-              </a>
-            </div>
+      <div className="grid bg-gray-100 pt-16 lg:pt-20 lg:pb-8 dark:bg-gray-700 lg:grid-cols-2 2xl:grid-cols-5 content-start">
+        <div className="mx-auto max-w-md px-4 py-4 sm:max-w-xl lg:max-w-full lg:px-12 lg:py-0 h-[28rem] xl:mr-0 2xl:col-span-2 border-4">
+          <h1 className="mt-2 text-2xl font-bold text-gray-900 dark:text-white sm:mt-4 sm:text-4xl lg:mt-4 lg:text-3xl xl:text-4xl">
+            You can travel anywhere.
+            <br />
+            <span className="text-indigo-500 dark:text-indigo-600">
+              Take advantage of that.
+            </span>
+          </h1>
+          <p className="mt-2 text-gray-600 dark:text-white sm:text-xl">
+            Tourcation lets you tour beautiful locations so you can have
+            enjoyable memories.
+          </p>
+          <img
+            src="/images/bus.jpg"
+            alt="People going in the bus"
+            className="my-2 h-52 w-full rounded-lg object-center shadow-xl sm:mt-8 sm:h-64 sm:object-cover lg:hidden"
+          />
+          <div className="mt-10 space-x-1 sm:mt-6">
+            <a
+              href="/"
+              className="focus:outline-none inline-block transform rounded-lg bg-indigo-500 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-white shadow-lg transition hover:-translate-y-0.5 hover:bg-indigo-400 focus:ring focus:ring-indigo-500 focus:ring-opacity-50 focus:ring-offset-2 active:bg-indigo-600 dark:bg-indigo-600  sm:text-base "
+            >
+              Join next tour
+            </a>
+            <a
+              href="/"
+              className="focus:outline-none inline-block rounded-lg bg-gray-300 px-5 py-3 text-sm font-semibold uppercase tracking-wider text-gray-800 hover:bg-gray-200 focus:ring focus:ring-gray-300 focus:ring-opacity-50 focus:ring-offset-2 active:bg-gray-400 sm:text-base"
+            >
+              Know more
+            </a>
+          </div>
         </div>
 
         <div className="relative m-3 hidden lg:block 2xl:col-span-3">
