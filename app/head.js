744a71d51ade8f50995603a93fba14daaf3222de
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-10 03:04:12 +0100

    addNext

---
+++ app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Tourcation</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="Tourcation app" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
