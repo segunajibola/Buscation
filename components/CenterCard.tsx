ca1d8986e8cd500d7c305d8379d13627fcd492db
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-16 13:38:49 +0100

    addSections

--- components/CenterCard.tsx
+++ components/CenterCard.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import React from "react";
-import { Link } from "next/link";
+import Link from "next/link";
 
 const CenterCard = ({ state, center }) => {
   return (
-    <Link to={`/${state}/${center.name.toLowerCase()}`} className="block">
+    <Link href={`/${state}/${center.name.toLowerCase()}`} className="block">
       <div className="flex h-[100%] transform cursor-pointer items-center overflow-hidden rounded-lg bg-gray-200 shadow-lg transition hover:-translate-y-2.5 hover:scale-105 dark:bg-gray-900">
         <img
           src={center.imageUrl}
