ce815979a26c1c1bf9896a43976214ccf203d2ba
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-22 02:25:28 +0100

    addSections

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { Inter } from "@next/font/google";
+// import { Inter } from "@next/font/google";
 import styles from "./page.module.css";
 // import Link from 'next/link'
 import { Hero } from "../sections";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
