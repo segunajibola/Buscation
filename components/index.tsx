8f126ee4f81f3c4d11e83eb98194c40ad19efb09
Segun Ajibola <ajbl.segun@gmail.com>
2023-04-22 03:58:13 +0100

    editmainv

--- components/index.tsx
+++ components/index.tsx
@@ -1,13 +1,11 @@
 import AllStatesCards from "./AllStatesCards"
 import CenterCard from "./CenterCard"
 import NotFound from "./NotFound"
-import ParticularCenter from "./ParticularCenter"
 import StateCard from "./StateCard"
 
 export {
     AllStatesCards,
     CenterCard,
     NotFound,
-    ParticularCenter,
     StateCard
 }
